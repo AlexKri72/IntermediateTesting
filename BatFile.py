ca633f1c5155435bc963f0e1af5271ad8e9d6103
AlexKri72 <krial11@rambler.ru>
2023-02-15 20:05:12 +0500

    all ready

--- BatFile.py
+++ BatFile.py
@@ -4,15 +4,50 @@ import tkinter as tk
 from tkcalendar import Calendar
 import CreateNote
 
-
 os.system("cls")
+
+
+def NewNote():
+    CreateNote.Note("", "", -1)
+
+
+def ReadFromFile(selectDate="NULL"):
+    global listNote
+    global NoteBox
+    with open("note.csv", "r") as file:
+        listNote = file.readlines()
+    if selectDate != "NULL":
+        listNote1 = []
+        for i in range(len(listNote)):
+            if listNote[i].split()[1] == selectDate:
+                listNote1.append(listNote[i])
+        NoteBox = Listbox(listvariable=Variable(value=listNote1))
+        NoteBox.grid(row=1, column=1, sticky="nsew", columnspan=3, padx=10)
+    else:
+        NoteBox = Listbox(listvariable=Variable(value=listNote))
+        NoteBox.grid(row=1, column=1, sticky="nsew", columnspan=3, padx=10)
+
+
+def ChangeNote():
+    selection = NoteBox.curselection()  # читаем позицию курсора на выбранной заметке
+    with open("note.csv", "r") as file:
+        listNote = file.readlines()
+    if not selection:  # если заметка не выбрана курсором, выдаем предупреждение
+        temp = -2
+        CreateNote.Note("", "", temp)
+    else:               # если заметка выбрана, вызываем функцию изменения
+        temp = listNote[selection[0]].split()
+        CreateNote.Note(
+            " ".join(temp[temp.index("Theme:")+1:temp.index("Note:")]), " ".join(temp[temp.index("Note:")+1:]), int(temp[0]))
 
 
 def DeleteNote():
     selection = NoteBox.curselection()
+    # удаляем на экране
     NoteBox.delete(selection[0])
+    # удаляем в файле
     listNote.pop(selection[0])
-
+    # записываем новый вариант файла
     with open("note.csv", "w") as file:
         file.writelines(listNote)
 
@@ -22,35 +57,41 @@ def Bat():
     window = tk.Tk()
     window.title("Заметки")
 
-    # add calendar and button "Select day"
+    # добавляем календарь и кнопку выбора даты
     cal = Calendar(window, selectmode='day', year=2023, month=2, day=1)
-    cal.grid(row=0, column=0, padx=20, pady=20, sticky=NW, rowspan=2)
+    cal.grid(row=0, column=0, padx=20,
+             pady=20, sticky=NW, rowspan=2)
 
     def grad_date():
         date.config(text="Выбрана дата: " + cal.get_date())
+        dateSelect = cal.get_date().split(".")
+        resultDate = dateSelect[2]+"-"+dateSelect[1]+"-"+dateSelect[0]
+        ReadFromFile(resultDate)
+
+    def deleteDate():
+        ReadFromFile("NULL")
 
     Button(window, text="Выбрать дату", command=grad_date).grid(
         row=2, column=0, padx=20, pady=5, sticky=N)
     date = Label(window, text="")
-    date.grid(row=3, column=0, padx=20, pady=20, sticky=N)
+    date.grid(row=4, column=0, padx=20, pady=20, sticky=N)
 
-    # add button
-    Button(text="Создать заметку", width=25, height=3, command=CreateNote.CreateNote).grid(row=0, column=1, padx=20, pady=20,
-                                                                                           sticky=N)
+    Button(window, text="Сбросить дату", command=deleteDate).grid(
+        row=3, column=0, padx=20, pady=5, sticky=N)
+
+    # добавляем кнопки
+    Button(text="Создать заметку", width=25, height=3, command=NewNote).grid(row=0, column=1, padx=20, pady=20,
+                                                                             sticky=N)
     Button(text="Изменить заметку", width=25,
-           height=3).grid(row=0, column=2, padx=20, pady=20, sticky=N)
+           height=3, command=ChangeNote).grid(row=0, column=2, padx=20, pady=20, sticky=N)
     Button(text="Удалить заметку", width=25,
            height=3, command=DeleteNote).grid(row=0, column=3, padx=20, pady=20, sticky=N)
+    Button(text="Обновить список", width=75,
+           height=3, command=ReadFromFile).grid(row=2, column=1, padx=5, pady=5, sticky=N, columnspan=3)
 
     for i in range(3):
         window.columnconfigure(i, weight=1, minsize=75)
         window.rowconfigure(i, weight=1, minsize=50)
-
-    with open("note.csv", "r") as file:
-        global listNote
-        listNote = file.readlines()
-    global NoteBox
-    NoteBox = Listbox(listvariable=Variable(value=listNote))
-    NoteBox.grid(row=1, column=1, sticky="nsew", columnspan=3, padx=10)
+    ReadFromFile()
 
     window.mainloop()
--- CreateNote.py
+++ CreateNote.py
@@ -1,41 +1,66 @@
 import datetime
+import os
 import tkinter as tk
 
 
-def CreateNote():
-
+def Note(text1, text2, number):
+    global num
+    num = number  # -2 -заметка не выбрана, -1 -создается новая, либо номер редактируемой заметки
     # Создается новое окно с заголовком
     global window1
     window1 = tk.Toplevel()
     window1.title("Новая заметка")
 
-    # Создает ярлык и текстовое поле для ввода названия темы.
-    lblNoteTheme = tk.Label(window1, text="Тема заметки:")
-    global entNoteTheme
-    global entNoteText
-    entNoteTheme = tk.Entry(window1, width=100)
-    lblNoteTheme.grid(row=0, column=0, sticky="w", padx=10, pady=20)
-    entNoteTheme.grid(row=0, column=1, sticky="w", padx=10, pady=20)
+    if number == -2:
+        tk.Label(window1, text="Выберите заметку, которую необходимо изменить").grid(
+            row=0, column=0, sticky="w", padx=10, pady=20)
 
-    # Создает ярлык и текстовое поле для ввода текста заметки.
-    lblNoteText = tk.Label(window1, text="Текст :")
-    entNoteText = tk.Text(window1, width=80, height=10)
-    lblNoteText.grid(row=1, column=0, sticky="w", padx=10, pady=20)
-    entNoteText.grid(row=1, column=1, sticky="w", padx=10, pady=20)
+    else:
 
-    # Создает кнопку "Сохранить"
-    tk.Button(window1, text="Сохранить", width=40, height=2, command=SaveToFile).grid(
-        row=2, column=1, padx=10, pady=20)
-    for i in range(3):
-        window1.columnconfigure(i, weight=1, minsize=120)
-        window1.rowconfigure(i, weight=1, minsize=100)
+        # Создает ярлык и текстовое поле для ввода названия темы.
+        lblNoteTheme = tk.Label(window1, text="Тема заметки:")
+        global entNoteTheme
+        global entNoteText
+        entNoteTheme = tk.Entry(window1, width=100)
+        lblNoteTheme.grid(row=0, column=0, sticky="w", padx=10, pady=20)
+        entNoteTheme.grid(row=0, column=1, sticky="w", padx=10, pady=20)
+        entNoteTheme.insert("0", text1)
+
+        # Создает ярлык и текстовое поле для ввода текста заметки.
+        lblNoteText = tk.Label(window1, text="Текст :")
+        entNoteText = tk.Text(window1, width=80, height=10)
+        lblNoteText.grid(row=1, column=0, sticky="w", padx=10, pady=20)
+        entNoteText.grid(row=1, column=1, sticky="w", padx=10, pady=20)
+        entNoteText.insert("1.0", text2)
+
+        # Создает кнопку "Сохранить"
+        tk.Button(window1, text="Сохранить", width=40, height=2, command=SaveToFile).grid(
+            row=2, column=1, padx=10, pady=20)
+        for i in range(3):
+            window1.columnconfigure(i, weight=1, minsize=120)
+            window1.rowconfigure(i, weight=1, minsize=100)
 
 
 def SaveToFile():
+
     with open("note.csv", "r+") as file:
         array = file.readlines()
-        file.write(str(len(array)+1)+" ")
-        file.write(str(datetime.datetime.now())[:-7]+" ")
-        file.write(str(entNoteTheme.get())+" ")
-        file.write(str(entNoteText.get("1.0", tk.END)))
+        if not array:
+            file.write("1 ")
+        else:
+            file.write(str(int(array[-1].split()[0])+1)+" ")
+            file.write(str(datetime.datetime.now())[:-7]+" ")
+            file.write(str("Theme: " + entNoteTheme.get())+" ")
+            file.write(str("Note: " + entNoteText.get("1.0", tk.END)))
+    if 0 <= int(num):
+        with open("note.csv", "r+") as file:
+            array = file.readlines()
+            for i in range(len(array)):
+                if int(array[i].split()[0]) == num:
+                    del array[i]
+                    break
+
+        with open("note.csv", "w+") as file:
+            file.writelines(i for i in array)
+
     window1.destroy()
